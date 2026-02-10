b4c09ff602010d387767620bd7dfe3ec4c0e47c9
Boo Pannachai <booya.pannachai@gmail.com>
2026-02-10 11:30:13 +0700

    initial commit

---
+++ MeVibe/MeVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeVibeApp.swift
+//  MeVibe
+//
+//  Created by Panachai Sulsaksakul on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct MeVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
